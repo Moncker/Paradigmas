e6d76469b9a162d1ac454e238601ccd0921cf670
Aps09 <Aps09@users.noreply.github.com>
2019-11-25 22:16:23 +0100

    creacion clase TestSUTLocalTiempoCoor.java

---
+++ src/test/java/core/SUT/TestSUTLocalTiempoCoor.java
@@ -0,0 +1,29 @@
+package core.SUT;
+
+import core.model.Estado;
+import core.model.Tiempo;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class TestSUTLocalTiempoCoor extends TestSUTBed {
+
+    @Test
+    public void demandaGradosCoordenadaValida() {
+        Tiempo tiempoTeo = new Tiempo(15.2, Estado.NUBLADO, 60);
+        sut.coordenadaTiempo(new Coordenadas(0.1, 1.2, 1.2, 1.3, 1.1, 1.1), new Date());
+
+        assertEquals(15.2, tiempoTeo.getGrados(), 0.05);
+    }
+
+    @Test
+    public void demandaGradosCoordenadaInvalida() {
+        Tiempo tiempoTeo = new Tiempo(15.2, Estado.NUBLADO, 60);
+        sut.coordenadaTiempo(new Coordenadas(0.1,1.2,1.2,1.3,1.1, 1.1), new Date());
+
+        assertNotEquals(21, tiempoTeo.getGrados(), 0.05);
+    }
+}
