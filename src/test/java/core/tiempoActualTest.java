da7293e034242edc58b05cf6b5f7d94939a72c44
Andrés <al341853@uji.es>
2019-12-05 16:39:45 +0100

    Actualizado con errores solucionados

--- src/test/java/core/tiempoActualTest.java
+++ src/test/java/core/tiempoActualTest.java
@@ -4,11 +4,13 @@ import core.model.Estado;
 import core.model.Tiempo;
 import org.junit.Test;
 
+import java.util.Date;
+
 public class tiempoActualTest {
     @Test
     public void tiempoActualFunctional(){
-        Tiempo resultadoReal = new Tiempo(0, Estado.LLUVIOSO, 31);
-        Tiempo resultadoEsperado = new Tiempo(32, Estado.SOLEADO, 32.5);
+        Tiempo resultadoReal = new Tiempo(0, Estado.LLUVIOSO, 31, new Date());
+        Tiempo resultadoEsperado = new Tiempo(32, Estado.SOLEADO, 32.5, new Date());
         throw new UnsupportedOperationException();
     }
 
