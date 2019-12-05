79ae25906f5c7b4f16cfe9f4f21b7882bab7daf5
user-pc <al258002@uji.es>
2019-12-05 19:03:30 +0100

    PosReunion_1

--- src/main/java/core/model/Coordenadas.java
+++ src/main/java/core/model/Coordenadas.java
@@ -2,20 +2,11 @@ package core.model;
 
 public class Coordenadas {
 
-    Double Xdegrees;
-    Double Xminutes;
-    Double Xseconds;
+    Double X;
+    Double Y;
 
-    Double Ydegrees;
-    Double Yminutes;
-    Double Yseconds;
-
-    public Coordenadas(Double xdegrees, Double xminutes, Double xseconds, Double ydegrees, Double yminutes, Double yseconds) {
-        Xdegrees = xdegrees;
-        Xminutes = xminutes;
-        Xseconds = xseconds;
-        Ydegrees = ydegrees;
-        Yminutes = yminutes;
-        Yseconds = yseconds;
+    public Coordenadas(Double x, Double y) {
+        X = x;
+        Y = y;
     }
 }
--- src/main/java/core/model/User.java
+++ src/main/java/core/model/User.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 public class User {
-    public int code;
+    public int code; //Codigo unico del cliente
     public LinkedList <Localizacion> favoritos; // Guardadas como localizaciones
     public HashMap<Localizacion, String> tags; // Solo aquí existe el nombrado propio del cliente
 
