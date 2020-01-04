6a38d9d21afaf67f21bfbe0f2f7f03923522de6b
NietakO <al316458@uji.es>
2020-01-04 16:02:56 +0100

    Funcionalidad por dias

--- src/main/java/core/view/TemperaturaVistaController.java
+++ src/main/java/core/view/TemperaturaVistaController.java
@@ -5,12 +5,9 @@ import core.OWM.App;
 import core.model.Tiempo;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 
 public class TemperaturaVistaController {
